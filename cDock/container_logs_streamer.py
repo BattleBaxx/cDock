fbd34473e8e232d0d2c567697079d1fb98e87798
RazCrimson <52282402+RazCrimson@users.noreply.github.com>
2021-11-16 01:13:25 +0530

    add: ContainerLogsStreamer

---
+++ cDock/container_logs_streamer.py
@@ -0,0 +1,22 @@
+from docker.models.containers import Container
+
+from cDock.container_info_streamer import ContainerInfoStreamer
+
+
+class ContainerLogsStreamer(ContainerInfoStreamer):
+
+    def __init__(self, container: Container):
+        super().__init__(container, sleep_interval=0)
+        self.logs: bytes = b''
+        self.last_update_timestamp = self.time_initialized
+
+    def get_stream_generator(self):
+        return self.container.logs(stream=True)
+
+    def stream_handler(self, streamed_value):
+        self.logs += streamed_value
+
+    def get_streamed_logs(self) -> str:
+        logs = self.logs
+        self.logs = b''
+        return logs.decode('utf-8')
